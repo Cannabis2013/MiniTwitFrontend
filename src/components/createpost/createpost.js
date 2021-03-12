4425e8cb51f28b4ed13c0534982300396251e139
Cannabis2013 <MH2012@outlook.dk>
2021-03-12 16:09:17 +0100

    Implemented fontfamily and fontsize selectors

--- src/components/createpost/createpost.js
+++ src/components/createpost/createpost.js
@@ -7,16 +7,45 @@ export default {
   props: [],
   data () {
     return {
-      userText : "",
-      isTransmitting : false,
-      selectedAccessMode : "4"
+        userText : "",
+        fontFamily : "Helvetica, sans-serif;",
+        fontSize : "17pt",
+        isTransmitting : false,
+        selectedAccessMode : "4",
+        fonts : [
+            "Arial, sans-serif",
+            "Helvetica, sans-serif",
+            "Gill Sans, sans-serif",
+            "Lucida, sans-serif",
+            "Helvetica Narrow, sans-serif",
+            "sans-serif",
+            "system-ui",
+            "Helvetica, sans-serif;",
+            "Arial",
+            "Sans Serif",
+            "Open Sans",
+            "Times, serif",
+            "Times New Roman",
+            "Palatino, serif",
+            "Bookman, serif",
+            "New Century Schoolbook, serif",
+            "serif"
+        ]
     }
   },
   computed: {
-    
+    computePointSizes : function(){
+        let pSizes = [];
+        for (let i = 1; i < 32; i++)
+            pSizes.push(i + "pt");
+        for(let i = 32;i <64;i += 4)
+            pSizes.push(i + "pt");
+        for(let i = 64;i <=128;i += 16)
+            pSizes.push(i + "pt");
+        return pSizes;
+    }
   },
   mounted () {
-
   },
   methods: {
     handleShare : function ()
@@ -34,7 +63,9 @@ export default {
             TokenId : tId,
             TokenAddress : TokenAddress,
             Text : this.userText,
-            Access : this.selectedAccessMode
+            Access : this.selectedAccessMode,
+            fontFamily : this.fontFamily,
+            fontSize : this.fontSize
         }
         }).then(response => {
             let payLoad = response.data;
